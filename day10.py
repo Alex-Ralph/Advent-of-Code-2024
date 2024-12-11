f123265503d4774e44a20c58f51da4c8ad201d84
Alexander Ralph <alexpr@btopenworld.com>
2024-12-11 14:10:53 +0000

    Add docstrings, type hints for days 10 and 11

--- day10.py
+++ day10.py
@@ -1,17 +1,20 @@
+from collections.abc import Callable, Generator
 data = open("input-data/day10.txt").read()
 park_map = {x + 1j * y: int(val) for y, line in enumerate(data.split("\n"))
        for x, val in enumerate(line)}
 
-map_width = len([x for x in park_map if x.real == 0])
-map_height = len([x for x in park_map if x.imag == 0])
+map_width = sum(1 for x in park_map if x.real == 0)
+map_height = sum(1 for x in park_map if x.imag == 0)
 
-def get_surroundings(coord):
+def get_surroundings(coord: complex) -> Generator[complex]:
+    """get in-bound coordinates of input coord"""
     surrounding_operations = [1j, -1j, 1, -1]
     for coord in [x + coord for x in surrounding_operations]:
         if 0 <= coord.real < map_width and 0 <= coord.imag < map_height:
             yield coord
 
-def check_paths(coord):
+def check_paths(coord: complex) -> Generator[complex]:
+    """For a given coordinate, finds all valid paths to a summit (9)"""
     surroundings = list(get_surroundings(coord))
     valid_moves = [x for x in surroundings if park_map[x] == park_map[coord] + 1]
     if not valid_moves:
@@ -25,26 +28,22 @@ def check_paths(coord):
                 continue
             yield end_node
 
-
-def get_trailhead_score(coord):
+def get_trailhead_score(coord: complex) -> int:
+    """Returns the count of unique summits reachable from the input coordinate"""
     end_nodes = list(check_paths(coord))
     unique_end_nodes = list(set(end_nodes))
     return len(unique_end_nodes)
 
-def get_trailhead_rating(coord):
+def get_trailhead_rating(coord: complex) -> int:
+    """Returns the count of all possible paths from the input coordinate to a summit"""
     end_nodes = list(check_paths(coord))
     return len(end_nodes)
 
-def part_x(func):
+def part_x(func: Callable[[complex], int]) -> int:
+    """Calls func on each starting coordinate in the map, returns the sum of func(starting_coordinate)"""
     trail_starts = [coord for coord in park_map.keys() if park_map[coord] == 0]
     scores = [func(x) for x in trail_starts]
     return sum(scores)
 
-def part_one():
-    return part_x(get_trailhead_score)
-
-def part_two():
-    return part_x(get_trailhead_rating)
-
-print(f"Part one: {part_one()}")
-print(f"Part two: {part_two()}")
+print(f"Part one: {part_x(get_trailhead_score)}")
+print(f"Part two: {part_x(get_trailhead_rating)}")
--- day11.py
+++ day11.py
@@ -6,6 +6,7 @@ stones = [int(x) for x in data.split()]
 
 @functools.cache
 def blink_at_stone(stone: int, generations: int):
+    """Returns the number of stones you'd get if you blinked for n generations, from one starting stone"""
     if generations == 0:
         return 1
     if stone == 0:
@@ -18,7 +19,8 @@ def blink_at_stone(stone: int, generations: int):
             new_stones = divmod(stone, pow(10, digits/2))
             return blink_at_stone(new_stones[0], generations-1) + blink_at_stone(new_stones[1], generations-1)
 
-def count_rocks(cycles):
+def count_rocks(cycles: int) -> int:
+    """Returns rock count for 'cycles' generations"""
     return sum(blink_at_stone(x, cycles) for x in stones)
 
 print(f"Part one: {count_rocks(25)}")
