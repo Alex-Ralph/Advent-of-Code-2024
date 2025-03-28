81a6e3a62e4e1a427898bf5d40361be320cec3f8
Alexander Ralph <alexpr@btopenworld.com>
2025-03-28 15:32:00 +0000

    Solve day 22 part 2

--- day22.py
+++ day22.py
@@ -1,5 +1,7 @@
+import line_profiler
+
 def parse_data():
-    with open("test-data/day22.txt") as file:
+    with open("input-data/day22.txt") as file:
         return [int(x) for x in file.read().split("\n")]
 
 def mix(val, secret):
@@ -15,6 +17,25 @@ def secret_generator(secret):
         secret = prune(mix(secret * 2048, secret))
         yield secret
 
+def get_price_changes(price_list: list[int]):
+    return  [(price_list[x-3] - price_list[x-4],
+            price_list[x-2] - price_list[x-3],
+            price_list[x-1] - price_list[x-2],
+            price_list[x] - price_list[x-1]) for x in range(4, len(price_list))]
+
+def create_secret_dict(secret: int):
+    gen = secret_generator(secret)
+    secrets = [secret] + [next(gen) for _ in range(1999)]
+    prices = [int(str(x)[-1]) for x in secrets]
+    price_changes = get_price_changes(prices)
+    relevant_prices = prices[4:]
+    secret_dict = {}
+    for id, changes in enumerate(price_changes):
+        if changes not in secret_dict:
+            secret_dict[changes] = relevant_prices[id]
+    return secret_dict
+
+@line_profiler.profile
 def part_one():
     secrets = parse_data()
     secret_sum = 0
@@ -25,43 +46,25 @@ def part_one():
         secret_sum += next(gen)
     print(secret_sum)
 
+@line_profiler.profile
 def part_two():
-    """ The dumbest way of solving part 2:
-    1. Create a dictionary for each secret, {pattern: value}
-    2. For each possible pattern, sum up the total
-    3. return the sum total for the best pattern
-    """
     secrets = parse_data()
     secret_moves_and_prices = []
     for secret in secrets:
-        secret_gen = secret_generator(secret)
-        secret_list = [secret]
-        secret_list += [next(secret_gen) for _ in range(1999)]
-        price_list = [int(str(x)[-1]) for x in secret_list]
-        price_moves = [(price_list[x-3] - price_list[x-4],
-                        price_list[x-2] - price_list[x-3],
-                        price_list[x-1] - price_list[x-2],
-                        price_list[x] - price_list[x-1]) for x in range(4, 2000)]
-        moves_and_prices = list(zip(price_moves, price_list[4:]))
-        secret_moves_and_prices.append(moves_and_prices)
+        secret_moves_and_prices.append(create_secret_dict(secret))
     possible_moves = [(a,b,c,d) for a in range(-9, 10) # this also contains many impossible moves
                       for b in range(-9, 10)
                       for c in range(-9, 10)
                       for d in range(-9, 10)]
     move_vals = []
-    i = 0
     for move in possible_moves:
-        move_val = 0
+        move_price = 0
         for x in secret_moves_and_prices:
-            for price_moves, price in x:
-                if price_moves == move:
-                    move_val += price
-                    continue
-        move_vals.append(move_val)
-        i += 1
-        print(i)
+            if move in x:
+                move_price += x[move]
+        move_vals.append(move_price)
     print(max(move_vals))
 
 
-# part_one()
+part_one()
 part_two()
