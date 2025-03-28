2db0fdc2fc4acaadccdd6b3d14cc6fd7e66b49ed
Alexander Ralph <alexpr@btopenworld.com>
2025-03-28 16:09:51 +0000

    Optimise day 22 pt 2

--- day22.py
+++ day22.py
@@ -23,7 +23,7 @@ def get_price_changes(price_list: list[int]):
             price_list[x-1] - price_list[x-2],
             price_list[x] - price_list[x-1]) for x in range(4, len(price_list))]
 
-def create_secret_dict(secret: int):
+def create_secret_list(secret: int) -> list[tuple[tuple[int], int]]:
     gen = secret_generator(secret)
     secrets = [secret] + [next(gen) for _ in range(1999)]
     prices = [int(str(x)[-1]) for x in secrets]
@@ -33,7 +33,7 @@ def create_secret_dict(secret: int):
     for id, changes in enumerate(price_changes):
         if changes not in secret_dict:
             secret_dict[changes] = relevant_prices[id]
-    return secret_dict
+    return [(key, value) for key, value in secret_dict.items()]
 
 @line_profiler.profile
 def part_one():
@@ -49,21 +49,16 @@ def part_one():
 @line_profiler.profile
 def part_two():
     secrets = parse_data()
-    secret_moves_and_prices = []
-    for secret in secrets:
-        secret_moves_and_prices.append(create_secret_dict(secret))
-    possible_moves = [(a,b,c,d) for a in range(-9, 10) # this also contains many impossible moves
+    move_prices = {(a,b,c,d): 0 for a in range(-9, 10) # this also contains many impossible moves
                       for b in range(-9, 10)
                       for c in range(-9, 10)
-                      for d in range(-9, 10)]
-    move_vals = []
-    for move in possible_moves:
-        move_price = 0
-        for x in secret_moves_and_prices:
-            if move in x:
-                move_price += x[move]
-        move_vals.append(move_price)
-    print(max(move_vals))
+                      for d in range(-9, 10)}
+    for secret in secrets:
+        secret_list = create_secret_list(secret)
+        for x in secret_list:
+            move_prices[x[0]] += x[1]
+    print(max(move_prices.values()))
+
 
 
 part_one()
