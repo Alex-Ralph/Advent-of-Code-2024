13f7f5483acf6cea2189bb6b9313ee2efe1d40aa
Alex Ralph <alexpr@btopenworld.com>
2025-04-10 19:19:57 +0200

    rework day 24 part one

--- day24.py
+++ day24.py
@@ -1,109 +1,70 @@
-from typing import Generator
 from copy import copy
-# This whole file is ugly and I don't like it. Would not push to prod.
+class Wire():
+    def __init__(self, inputs: list[str], output: str, operation: str) -> None:
+        self.inputs = inputs
+        self.output = output
+        self.operation = operation
+        self.value = None
 
-def parse_data() -> tuple[dict[str: bool|None], list[list[str]]]:
+    def solve(self, gates):
+        input_values = tuple((gates[x] for x in self.inputs))
+        match self.operation:
+            case "OR":
+                if any(input_values):
+                    self.value = True
+                elif all((x is False for x in input_values)):
+                    self.value = False
+            case "AND":
+                if all(input_values):
+                    self.value = True
+                if False in input_values:
+                    self.value = False
+            case "XOR":
+                if True in input_values and False in input_values:
+                    self.value = True
+                elif not None in input_values:
+                    self.value = False
+        if self.value is not None:
+            gates[self.output] = self.value
+            return True
+        return False
+            
+def parse_data() -> tuple[dict[str: bool], tuple[Wire]]:
     with open("test-data/day24.txt") as file:
         lines = file.read()
         init_lines, wire_lines = lines.split("\n\n")
         init_lines = init_lines.split("\n")
         wire_lines = wire_lines.split("\n")
-        init_vals = [x.split(": ") for x in init_lines]
-        wires = [x.split(" ") for x in wire_lines]
-        for wire in wires:
-            wire.remove("->")
-        unique_pins = set([x[0] for x in init_vals] + [wire[0] for wire in wires] + [x for wire in wires for x in (wire[0], wire[2], wire[3])])
-        pin_maps = {pin: None for pin in unique_pins}
-        for val in init_vals:
-            pin_maps[val[0]] = bool(int(val[1]))
-        return pin_maps, wires
-
-def solve_wire(wire_string: list[str], pin_maps: dict[str: bool|None]):
-    left_pin, op, right_pin, out = wire_string
-    lhs = pin_maps[left_pin]
-    rhs = pin_maps[right_pin]
-    match op:
-        case "OR":
-            if any((lhs, rhs)):
-                pin_maps[out] = True
-                return True
-            if lhs is False and rhs is False:
-                pin_maps[out] = False
-                return True
-        case "AND":
-            if all((lhs, rhs)):
-                 pin_maps[out] = True
-                 return True
-            if False in ((lhs, rhs)):
-                pin_maps[out] = False
-                return True
-        case "XOR":
-            if True in ((lhs, rhs)) and False in ((lhs, rhs)):
-                pin_maps[out] = True
-                return True
-            if not None in ((lhs, rhs)):
-                pin_maps[out] = False
-                return True
-
-def solve(wires: list[list[[str]]], pin_maps: dict[str: bool|None]) -> None:
-    """Edits pin_maps in place"""
-    while wires:
-        pins_solved = [(x, solve_wire(x, pin_maps)) for x in wires]
-        for x in pins_solved:
-            if x[1]:
-                wires.remove(x[0])
+        gate_dict = {x: bool(int(y)) for a in init_lines for x, y in [a.split(": ")]}
+        wire_values = [x.split(" ") for x in wire_lines]
+        for x in wire_values:
+            gate_dict[x[-1]] = None
+        wires = []
+        for wire_val in wire_values:
+            wire_val.remove("->")
+            wires.append(Wire((wire_val[0], wire_val[2]), wire_val[-1], wire_val[1]))
+        return gate_dict, wires
 
 def part_one():
-    pin_maps, wires = parse_data()
-    solve(wires, pin_maps)
+    gates, wires = parse_data()
     while wires:
-        pins_solved = [(x, solve_wire(x, pin_maps)) for x in wires]
-        for x in pins_solved:
-            if x[1]:
-                wires.remove(x[0])
-    z_list = [str(int(pin_maps[x])) for x in sorted(pin_maps.keys(), reverse=True) if x[0] == "z"]
-    print(int("".join(z_list), 2))
-
-def swap_wires(wires: list[list[str]]):
-    for i in range(len(wires) - 1):
-        for j in range(i+1, len(wires)):
-            a=wires[i][3]
-            wires[i][3] = wires[j][3]
-            wires[j][3] = a
-            yield (wires[i][3], wires[j][3])
-            wires[j][3] = wires[i][3]
-            wires[i][3] = a
-    yield (None, None)
+        solved_wires = [x for x in wires if x.solve(gates) == True]
+        for x in solved_wires:
+            wires.remove(x)
+    z_values = [x for x in gates if x[0] == 'z']
+    z_values.sort(reverse=True)
+    binary = "".join(str(int(gates[x])) for x in z_values)
+    print(int(binary, 2))
 
 def part_two():
-    """Grug solution: just keep swapping shit until it works
-    Potentially better solution: Find out which wire will change which binary bits, and which binary bits need changing
-    Start swapping the pins that will influence the correct binary bits
-    Grug solution will be helpful for producing a method of swapping wires, so start with that and when it
-    inevitably fails, try being smarter about it
-    """
-    pin_maps, wires = parse_data()
-    for swaps in swap_wires(wires):
-        for swaps_2 in swap_wires(wires):
-            if any((x in swaps_2 for x in swaps)):
+    gates, wires = parse_data()
+    SWAPPED_WIRES = 2
+    for wire_one in wires:
+        wire_one_init = wire_one.output
+        for wire_two in wires:
+            if wire_one == wire_two:
                 continue
-            result = copy(pin_maps)
-            solve(copy(wires), result)
-            x_list = [result[x] for x in sorted(result.keys(), reverse=True) if x[0] == "x" and result[x] is not None]
-            y_list = [result[x] for x in sorted(result.keys(), reverse=True) if x[0] == "y" and result[x] is not None]
-            z_list = [result[x] for x in sorted(result.keys(), reverse=True) if x[0] == "z" and result[x] is not None]
-            if [] in [x_list, y_list, z_list]:
-                continue
-            x_strs = [str(int(x))for x in x_list]
-            y_strs = [str(int(x)) for x in y_list]
-            z_strs = [str(int(x)) for x in z_list]
+            wire_one.output = wire_two.output
+            wire_two.output = wire_one_init
 
-            x_val = int("".join(x_strs), 2)
-            y_val = int("".join(y_strs), 2)
-            z_val = int("".join(z_strs), 2)
-            if x_val & y_val == z_val:
-                print(swaps, swaps_2)
-                return
 
-part_one()
-part_two()
